46f2a1f12c5ca1e5b9041c6b2a2e82a79f15c5d0
Gérald Arnould <gerald@agharta.fr>
2024-01-29 09:25:12 +0100

    feat: get and display base URL

--- crawl.js
+++ crawl.js
@@ -6,5 +6,47 @@ function normalizeURL(url) {
         return (urlObj.hostname + urlObj.pathname.replace(/\/+$/, ''));
 }
 
-export default normalizeURL;
+async function crawlPage(url) {
+        try {
+                const resp = await fetch(url, {
+                        method: 'GET',
+                        mode: 'cors',
+                        // headers: {
+                        //
+                        // },
+                });
+
+                // Deal with return codes
+                if (resp.status >= 300 && resp.status <= 399) {
+                        console.error(`Redirection. Status: ${resp.status}`);
+                        return;
+                } else if (resp.status >= 400 && resp.status <= 499) {
+                        console.error(`Client error. Status ${resp.status}`);
+                        return;
+                } else if (resp.status >= 500) {
+                        console.error(`Server error. Status ${resp.status}`);
+                }
+
+                // Content Type
+                // console.error(`Headers: )`);
+                // for (const header of resp.headers.entries()) {
+                //         console.log(`- ${header}`);
+                // }
+                if (!resp.headers.get('content-type').includes('text/html')) {
+                        console.error(`Unable to parse ${resp.headers.get('content-type')}`);
+                        return;
+                }
+                const textBody = await resp.text();
+                console.log(textBody);
+        } catch (err) {
+                console.log(`Couldn't fetch ${url}: ${err.message}`);
+                return;
+        }
+
+}
+
+export {
+        normalizeURL,
+        crawlPage,
+};
 
